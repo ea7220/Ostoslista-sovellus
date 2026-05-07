9523cfecdbcacb684ad574a1f51f5c09b942fc49
Tran Dinh <s2401130@edu.bc.fi>
2026-05-07 10:17:45 +0300

    Lisää ItemList komponentti

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import Header from "./tehtava/Header";
+import ItemList from "./tehtava/ItemList";
 
 function App() {
+  const testi = ["omena", "banaani", "kiivi"];
+
   return (
     <div>
       <Header />
+
+      <ItemList items={testi} />
     </div>
   );
 }
